a5aacc772531d3ad3e3e0732d527bef4be71b93f
furuhama <furuhama@users.noreply.github.com>
2018-10-28 17:37:52 +0900

    Set form to put an url

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class RootsController < ApplicationController
+  before_action :set_url, only: %i[show]
+
   def show
+  end
+
+  private
+
+  def set_url
+    @url = Url.new
   end
 end
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -12,11 +12,20 @@ class UrlsController < ApplicationController
   end
 
   def create
+    if url = Url.create(direction: url_param[:direction])
+      redirect_to root_path, notice: "Here's Warp gate for your direction: #{url.hashed_value}"
+    else
+      redirect_to root_path, alert: 'Failed to generate shorten url'
+    end
   end
 
   private
 
   def set_url
     @url = Url.find_by(code: params[:code])
+  end
+
+  def url_param
+    params.permit(:direction)
   end
 end
