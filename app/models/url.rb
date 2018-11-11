c17d4520914b5104405182a4097b05e39f336a6a
furuhama <furuhama@users.noreply.github.com>
2018-11-11 17:39:47 +0900

    Add uniqueness validation for Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Url < ApplicationRecord
-  validates :direction, presence: true, length: { maximum: 255 }
-  validates :hashed_value, presence: true
+  validates :direction, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :hashed_value, presence: true, uniqueness: true
 end
