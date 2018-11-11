3f6fefd490db4e5a414ea0f98792521c7240a481
furuhama <furuhama@users.noreply.github.com>
2018-11-11 17:38:05 +0900

    Sanitize given url & Edit error control flow

--- app/models/url/generator.rb
+++ app/models/url/generator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'digest'
+require 'uri'
 
 class Url
   class Generator
@@ -9,22 +10,40 @@ class Url
     attr_reader :direction, :hashed_value
 
     def initialize(url)
-      @direction = url
-      @hashed_value = Digest::MD5.base64digest url
+      @given_url = url
     end
 
     def generate
+      if url.nil?
+        errors.add(:base, 'Url cannot be blank')
+
+        return false
+      end
+
+      @direction = sanitize_url(direction)
+      @hashed_value = Digest::MD5.base64digest(url)
+
       url = Url.new(to_hash)
 
-      if url.valid?
-        # NOTE: valid でかつ永続化に失敗する場合
-        # 予期せぬ状態になることが考えられるので例外を投げる
-        url.save!
-      else
-        errors.add(:base, 'Failed to generate shorten url')
+      # NOTE: valid でかつ永続化に失敗する場合
+      # 予期せぬ状態になることが考えられるので例外を投げる
+      url.save!
 
-        false
-      end
+      true
+    rescue URI::Error => e
+      errors.add(:base, 'Invalid url')
+
+      false
+    end
+
+    private
+
+    def sanitize_url(url)
+      url = URI.parse(URI.escape(url))
+
+      raise URI::InvalidURIError if url.scheme.nil? || url.host.nil?
+
+      url.scheme + url.host + url.port + url.path + url.query
     end
 
     def to_hash
