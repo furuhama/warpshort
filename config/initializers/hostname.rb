72ab47b55016fa136c8b49da1d0a9ff79c64951c
furuhama <furuhama@users.noreply.github.com>
2018-11-11 17:07:54 +0900

    Create hostname.rb

---
+++ config/initializers/hostname.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module WarpShort
+  HOSTNAME = if Rails.env.development?
+               'http://localhost:3000'.freeze
+             else
+               '' # TODO: Fill this string with production server hostname
+             end
+end
